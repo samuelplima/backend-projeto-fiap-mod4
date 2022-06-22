923b92895b98cea41c52b4d8d1e489b6bd55a890
Samuel Lima <samuel.lima@voffice.com.br>
2022-06-21 22:29:19 -0300

    feat : created app modules

---
+++ schedule/src/test/java/com/fiap/schedule/ScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
